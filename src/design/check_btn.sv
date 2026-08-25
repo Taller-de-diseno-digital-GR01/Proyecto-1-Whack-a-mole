0540830d8d662e5df9a55c42e1d4dae1811f275a
Jefferson Chinchilla <jechinchilla@estudiantec.cr>
2026-08-25 13:31:54 -0600

    mod check_btn

--- src/design/check_btn.sv
+++ src/design/check_btn.sv
@@ -6,13 +6,6 @@ module check_btn (
     output logic miss
 );
     //Usaré dos señales que me permite verificar si funcionan las señales de entrada de los botones.
-<<<<<<< HEAD
-    //Asigno hit que solo será 1 si se selecciono un botón y es igual a ala posición
-    //Asigno fail que solo será 1 si se falla la posición pero si existe un botón presionado.
-    always_comb begin
-        hit = valid_in && (enc_btn_in == pos_topo);
-        miss = valid_in && (enc_btn_in != pos_topo);
-=======
     //btn_valid: verifica que la señal calza con la posición asignada al topo
     //right_btn: proviene del encoder después de verificar si se presionó o no un botón
     //miss: se presionó un botón válido (valid_in) pero que NO corresponde a la posición del topo
@@ -27,7 +20,6 @@ module check_btn (
             valid = 1'b0;
             miss  = 1'b0;
         end
->>>>>>> develop
     end
 
 
