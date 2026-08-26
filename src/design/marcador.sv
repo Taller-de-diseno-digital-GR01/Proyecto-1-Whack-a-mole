23847ac92ae62535d986a029390e2e956877275f
mcit39 <mattioc@protonmail.com>
2026-08-26 17:18:57 -0600

    ternario de sel a if/else en marcador

--- src/design/marcador.sv
+++ src/design/marcador.sv
@@ -28,7 +28,12 @@ module marcador #(
   logic [1:0] sel;
 
   always_ff @(posedge clk) begin
-    sel <= rst ? 0 : (tick ? sel + 1 : sel);
+    if (rst)
+      sel <= 0;
+    else if (tick)
+      sel <= sel + 1;
+    else
+      sel <= sel;
   end
 
   // an3 an2 muestran acierto (decenas, unidades), an1 an0 muestran fallo (decenas, unidades)
